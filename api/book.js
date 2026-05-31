0e5c6a5ac0ad227999e74f41565f02dbd798909f
nekomura-ai <fumi4539@gmail.com>
2026-05-31 14:16:09 +0900

    Route birthday job through existing book API

--- api/book.js
+++ api/book.js
@@ -1,4 +1,11 @@
 const { adapt } = require("../lib/vercel-adapter");
-const { handler } = require("../netlify/functions/book");
+const { handler: bookHandler } = require("../netlify/functions/book");
+const { handler: birthdayMailsHandler } = require("../netlify/functions/birthday-mails");
 
-module.exports = adapt(handler);
+const handleBook = adapt(bookHandler);
+const handleBirthdayMails = adapt(birthdayMailsHandler);
+
+module.exports = async function handler(req, res) {
+  if (req.query?.job === "birthday-mails") return handleBirthdayMails(req, res);
+  return handleBook(req, res);
+};
